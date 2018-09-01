1137625a8620799d2e6a2e9ac5f302fac9bf5f2f
unknown <friends_1992_2009@hotmail.com>
2018-09-01 17:07:07 +0900

    change

--- sendmessage.py
+++ sendmessage.py
@@ -1,10 +1,10 @@
-import requests
+import requests, json
 ACCESS_TOKEN = "kIq31WcoZftFtqrh4r1a8jb3t0WcJ8pacLkXrvFawAGEUt8U3hEEwRxJlgAMgCCKnHL8x5vMvo8W+NkIuoUdHJ7pB7hupj2Oe501YfwMYvxXFd9jtrid8PhY9hS6NfHep3gXkpIXMBJ/I/NqfsAwYQdB04t89/1O/w1cDnyilFU="
 
 def Sendmessage():
     url = 'https://api.line.me/v2/bot/message/push'
     data = {
-        "to": "5311098b76a7d83cd2ea249221846e4b",
+        "to": "lvzhongyi",
         "messages": [
             {
                 "type": "text",
@@ -14,7 +14,7 @@ def Sendmessage():
     }
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Chenjianqi ' + ACCESS_TOKEN
+        'Authorization': 'Bearer' + ACCESS_TOKEN
     }
     requests.post(url, data=json.dumps(data), headers=headers)
 
